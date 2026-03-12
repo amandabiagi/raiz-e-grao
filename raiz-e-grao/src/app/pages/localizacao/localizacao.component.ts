3b4ed360f73cdca44b82b2243e79d98404ac672d
amandabiagi <amanda.biagi@bandtech.com.br>
2026-03-11 22:08:21 -0300

    Adicionando componentes principais

---
+++ raiz-e-grao/src/app/pages/localizacao/localizacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localizacao',
+  imports: [],
+  templateUrl: './localizacao.component.html',
+  styleUrl: './localizacao.component.scss'
+})
+export class LocalizacaoComponent {
+
+}
